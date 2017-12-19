7cba53c568e7e15604a8aca19b8f1d415064caf2
Simon Karlen <karlen@tonic.ag>
2017-12-19 14:33:26 +0100

    check if null

--- views/administration/_log-data.php
+++ views/administration/_log-data.php
@@ -10,19 +10,27 @@ use yii\widgets\DetailView;
 	<div class="col-xs-6">
 		<h3>Data before</h3>
 		<?php
-		echo DetailView::widget([
-			'model'      => $model->data_before,
-			'attributes' => array_keys($model->data_before)
-		])
+		if (!is_null($model->data_before)) {
+			echo DetailView::widget([
+				'model'      => $model->data_before,
+				'attributes' => array_keys($model->data_before)
+			]);
+		} else {
+			echo Yii::t('yii', '(not set)');
+		}
 		?>
 	</div>
 	<div class="col-xs-6">
 		<h3>Data after</h3>
 		<?php
-		echo DetailView::widget([
-			'model'      => $model->data_after,
-			'attributes' => array_keys($model->data_after)
-		]);
+		if (!is_null($model->data_after)) {
+			echo DetailView::widget([
+				'model'      => $model->data_after,
+				'attributes' => array_keys($model->data_after)
+			]);
+		} else {
+			echo Yii::t('yii', '(not set)');
+		}
 		?>
 	</div>
 </div>
